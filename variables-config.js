c3c2e533dcfd75191a9833d12abfe69ea67dae17
lyovabo <minasyanlevon@gmai.com>
2017-04-25 14:46:42 +0400

    code fix

--- variables-config.js
+++ variables-config.js
@@ -5,3 +5,12 @@ var variables = scssToJson('./bootstrap/bootstrap/_variables.scss');
 console.log(variables)
 var str = JsonToSass(variables);
 fs.writeFileSync('./configs/variables_test.json', JSON.stringify(variables),function(){});
+
+
+function JsonToSass(sassObj) {
+  var parsedVariables = '';
+  for (var key in sassObj) {
+    parsedVariables += key + ':' + sassObj[key] + ';' + '\n';
+  }
+  return parsedVariables;
+}
